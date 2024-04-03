3fe4343c099c1a2460cf1c4052af703ae3d61cc9
QZLin <qzlin01@163.com>
2024-04-03 10:24:28 +0800

    Deployed d8a4580 with MkDocs version: 1.5.3

--- js/theme_extra.js
+++ js/theme_extra.js
@@ -6,32 +6,3 @@
  */
 
 $('div.rst-content table').addClass('docutils');
-
-function foldAllNav() {
-    document.querySelectorAll(".toctree-expand").forEach(
-        v => {
-            if (v.parentElement.ariaExpanded != "false")
-                v.click()
-        }
-    )
-}
-
-function waitElementExist(selector) {
-    return new Promise(resolve => {
-        if (document.querySelector(selector))
-            return resolve(document.querySelector(selector));
-        const observer = new MutationObserver(function () {
-            if (document.querySelector(selector)) {
-                resolve(document.querySelector(selector));
-                observer.disconnect();
-            }
-        });
-
-        observer.observe(document.body, {
-            subtree: true,
-            childList: true,
-        });
-    });
-}
-
-waitElementExist('.toctree-expand').then(foldAllNav);
